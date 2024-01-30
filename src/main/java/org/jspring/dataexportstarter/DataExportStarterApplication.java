d894999bb2061675ecadfa68d2cd44b7a8eb7e6c
daniele.gubbiotti <gubbiottidaniele@gmail.com>
2024-01-30 11:35:30 +0100

    Initial commit

---
+++ src/main/java/org/jspring/dataexportstarter/DataExportStarterApplication.java
@@ -0,0 +1,13 @@
+package org.jspring.dataexportstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExportStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExportStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jspring/dataexportstarter/DataExportStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jspring.dataexportstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExportStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
